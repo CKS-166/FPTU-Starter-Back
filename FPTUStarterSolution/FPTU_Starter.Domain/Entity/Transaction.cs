7dde7061a640147947c41c9dd86dea9c233c4e82
baocd2003 <BaoCao494@gmail.com>
2024-06-03 18:57:08 +0700

    up

--- FPTUStarterSolution/FPTU_Starter.Domain/Entity/Transaction.cs
+++ FPTUStarterSolution/FPTU_Starter.Domain/Entity/Transaction.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FPTU_Starter.Domain.Entity
 {
@@ -12,6 +13,9 @@ namespace FPTU_Starter.Domain.Entity
 
         public Guid PackageId { get; set; }
         public string Description { get; set; }
+
+        [Range(0, (double)decimal.MaxValue)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal TotalAmount { get; set; }
         public string TransactionType { get; set; }
         public DateTime CreateDate { get; set; }
